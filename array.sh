4fba9d4ad19db8768991bda9b09d1dd8e96e0ffc
venugopalsantosh <venugopalsantosh.p@hotmail.com>
2024-08-26 20:19:11 +0530

    my files

---
+++ array.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+FRUITS=("Apple","Mango","Grapes")
+
+echo "print the fruit : ${FRUITS[0]}"
+echo "print all fruits : ${FRUITs[@]}"
---
+++ username.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "enter your username : "
+read -s USERNAME
+echo "your username is : $USERNAME"
+
+echo "enter your password : "
+read -s PASSWORD
+echo "your password is : $PASSWORD"
