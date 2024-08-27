af4c72ca5b1256c2704a5d10a3fa2232298339ee
venugopalsantosh <venugopalsantosh.p@hotmail.com>
2024-08-27 13:12:53 +0530

    my files

---
+++ colors.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+USER=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+ROOT_USER()
+{
+    if [ $USER -ne 0 ]
+echo "user needs root user privilages"
+exit 1
+fi
+
+}
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    echo  -e "Installation is  $R unsuccessful for $2  $N"
+    exit 1
+    echo -e "Installation is $G sucessful for $2  $N"
+
+}
+
+ROOT_USER
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+echo "GIT is not installed, going to install"
+dnf install git -y
+VALIDATE &? "GIT"
+else
+echo "GIT is already installed"
+fi
+
+dnf list installed mysql
+if [ $? ne 0 ]
+echo "mysql is not installed , going to install"
+dnf install mysql -y
+VALIDATE $? "MYSQL"
+else
+echo "mysql is already installed"
+fi
---
+++ function.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+USER=$(id -u)
+if [ $USER -ne 0 ]
+echo "user needs root user privilages"
+exit 1
+fi
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    echo "Installation is not successful for $2"
+    exit 1
+    echo "Installation is sussceful for $2"
+
+}
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+echo "GIT is not installed, going to install"
+dnf install git -y
+VALIDATE &? "GIT"
+else
+echo "GIT is already installed"
+fi
+
+dnf list installed mysql
+if [ $? ne 0 ]
+echo "mysql is not installed , going to install"
+dnf install mysql -y
+VALIDATE $? "MYSQL"
+else
+echo "mysql is already installed"
+fi
