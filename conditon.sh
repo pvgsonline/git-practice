354ebccc9d0c6a76fbde1e58154dcbf2197ed952
venugopalsantosh <venugopalsantosh.p@hotmail.com>
2024-08-27 12:39:23 +0530

    my files

---
+++ conditon.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUMBER=$1
+
+read $NUMBER
+
+if [ $NUMBER1 -gt 20 ]
+then 
+echo "Number is greater than 20"
+else
+echo "Number is not greater than 20"
+fi
---
+++ installation.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+USER=$(id -u)
+if [ $USER -ne 0 ]
+echo "user needs root user privilages"
+exit 1
+fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+echo "GIT is not installed, going to install"
+dnf install git -y
+if [ $? -ne 0 ]
+echo "GIT installation is not sucessful"
+exit 1
+else
+echo "GIT is installed succesfully"
+fi
+else
+echo "GIT is already installed"
+fi
+
+dnf list installed mysql
+if [ $? ne 0 ]
+echo "mysql is not installed , going to install"
+dnf install mysql -y
+if [ $? ne 0 ]
+echo "mysql installation is not successful"
+exit 1
+else
+echo "mysql installation is successful"
+fi
+else
+echo "mysql is already installed"
+fi
