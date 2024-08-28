9a1c1824571603d2d69f0ec343b0225e7b66532e
venugopalsantosh <venugopalsantosh.p@hotmail.com>
2024-08-28 16:29:04 +0530

    my files

--- redirection.sh
+++ redirection.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 USERID=$(u -id)
-$R="\e[31m"
-$G="\e[32m"
-$Y="\e[33m"
-$N="\e[0m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 LOG_FOLDER="/var/logs/shell-script"
 SCRIPTNAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIMESTAMP" 
-mkdir -p &LOG_FOLDER
+mkdir -p $LOG_FOLDER
 
 
 ROOT_USER()
@@ -35,6 +35,7 @@ for package in $@
 do
 dnf list installed $package &>>$LOG_FILE
 if [ $? -ne 0 ]
+then
 echo "$package is not installed" | tee -a $LOG_FILE
 dnf install $package -y &>>$LOG_FILE
 validate $? "$package" 
