e381d8b67177df2aae1020614bb09abf6c6ed58c
venugopalsantosh <venugopalsantosh.p@hotmail.com>
2024-09-01 21:32:44 +0530

    my files

--- countofwords.sh
+++ countofwords.sh
@@ -4,8 +4,13 @@ FILE=$(cat /home/ec2-user/assignement.txt)
 
 #grep -wc 
 
-while IFS= read -r line
-do
+#while IFS= read -r line
+#do
 #grep -o $line | wc -l
-echo "$line"
-done <<< $FILE
+#echo "$line"
+#done <<< $FILE
+
+for word in $(<$FILE)
+do
+echo "$word"
+done
